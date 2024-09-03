7720f7a2d926de6d4abd1137429c389fbfc40e66
h-masano <hiroki.masanon@gmail.com>
2024-09-03 23:42:12 +0900

    add lo in module-a

--- workspace-1/module-a/go.mod
+++ workspace-1/module-a/go.mod
@@ -2,4 +2,7 @@ module github.com/h-masano/go-workspace-sample/workspace-1/module-a
 
 go 1.23.0
 
-require github.com/h-masano/go-workspace-sample/workspace-1/module-b v0.0.0-20240901143940-af9e5cc063a6
+require (
+	github.com/samber/lo v1.46.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- workspace-1/module-a/main.go
+++ workspace-1/module-a/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/h-masano/go-workspace-sample/workspace-1/module-b"
+	// "github.com/h-masano/go-workspace-sample/workspace-1/module-b"
+	"runtime/debug"
+	"github.com/samber/lo"
 )
 
 func main() {
-	b := modb.ModB()
-	fmt.Println("run moda, import ", b)
+	// b := modb.ModB()
+	// fmt.Println("run moda, import ", b)
+	lo.ToPtr("module-a")
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Printf("Failed to read build info")
+		return
+	}
+	fmt.Println("----------------------module-a----------------------")
+	for _, dep := range bi.Deps {
+		fmt.Printf("Dep: %+v\n", dep)
+	}
 }
