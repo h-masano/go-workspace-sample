93f927bd651463e418ce849160f10714932f83e3
h-masano <hiroki.masanon@gmail.com>
2024-09-03 23:57:19 +0900

    Revert "add lo in module-a"
    This reverts commit 7720f7a2d926de6d4abd1137429c389fbfc40e66.

--- workspace-1/module-a/go.mod
+++ workspace-1/module-a/go.mod
@@ -2,7 +2,4 @@ module github.com/h-masano/go-workspace-sample/workspace-1/module-a
 
 go 1.23.0
 
-require (
-	github.com/samber/lo v1.46.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-)
+require github.com/h-masano/go-workspace-sample/workspace-1/module-b v0.0.0-20240901143940-af9e5cc063a6
--- workspace-1/module-a/main.go
+++ workspace-1/module-a/main.go
@@ -2,25 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	// "github.com/h-masano/go-workspace-sample/workspace-1/module-b"
-	"runtime/debug"
-	"github.com/samber/lo"
+	"github.com/h-masano/go-workspace-sample/workspace-1/module-b"
 )
 
 func main() {
-	// b := modb.ModB()
-	// fmt.Println("run moda, import ", b)
-	lo.ToPtr("module-a")
-
-	bi, ok := debug.ReadBuildInfo()
-	if !ok {
-		log.Printf("Failed to read build info")
-		return
-	}
-	fmt.Println("----------------------module-a----------------------")
-	for _, dep := range bi.Deps {
-		fmt.Printf("Dep: %+v\n", dep)
-	}
+	b := modb.ModB()
+	fmt.Println("run moda, import ", b)
 }
